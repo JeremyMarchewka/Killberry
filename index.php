ae3bb2439eba54618f7c91d5ed2b565e1b516b36
Chloé Servières <c.servieres@simplon-charleville.fr>
2019-04-25 14:58:49 +0200

    début de mise en place

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Accueil Kilberry</title>
+
+  <link href="css/reset.css" type="text/css" rel="stylesheet"/>
+  <link href="css/index.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<body>
+  <header>
+  <?php include "header.php"?>
+  </header>
+  <main>
+    <div class="parallax-window" data-parallax="scroll" data-image-src=""
+    alt="">
+  </main>
+  <footer>
+    <?php include "footer.php"?>
+  </footer>
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <!-- Pour la parallax -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+  <!-- Nos fichiers js -->
+  <script type="text/javascript" src=""></script>
+</body>
+</html>
