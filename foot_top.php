a68dc75f7183a7f6201b8dcd88ca7249f684594f
ClowClow <c.servieres@simplon-charleville.fr>
2019-05-03 14:05:03 +0200

    ajout header et footer jérémy + remise des fichiers en php

---
+++ foot_top.php
@@ -0,0 +1,13 @@
+<!----------------------------------------FOOTER--------------------------------------->
+<link rel="stylesheet" href="css/navfoot.css">
+<footer>
+    <section id="foot2">
+        <div class="contact">
+            <a href="https://fr-fr.facebook.com/lekilberry/" style="font-size: 2em; color: rgb(0, 0, 0)"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://twitter.com/KilberryPub?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" style="font-size: 2em; color: rgb(0, 0, 0)"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.yelp.fr/biz/the-kilberry-reims" style="font-size: 2em; color: rgb(0, 0, 0)"><i class="fab fa-yelp"></i></a>
+        </div>
+    </section>
+</footer>
+
+<div><a id="cRetour" class="cInvisible" href="#haut"></a></div>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+
+  <link rel="stylesheet" href="css/navfoot.css">
+
+  <header id="header">
+    <div class="container">
+        <nav id="nav-bar">
+            <img class="logo"  src="img/logo3.png" alt="" id="header-img">
+            <input type="checkbox" id="toggle">
+            <label for="toggle"></label>
+            <ul class="menu">
+                <li><a class="nav-link" href="#header"><span>Home</span></a></li>
+                <li><a class="nav-link" href="#talk-to-us">Contact Us</a></li>
+                <li><a class="nav-link" href="#media">Media</a></li>
+                <li><a class="nav-link" href="#promo">Promo</a></li>
+            </ul>
+        </nav>
+        </div>
+</header>
